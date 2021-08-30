d57973f073e5244dbdf3933c75db742a65f3660a
Harsh <jhah63448@gmail.com>
2021-08-30 20:24:09 +0530

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
 import logo from './logo.svg';
+import './bootstrap.min.css';
 import './App.css';
+import React,{ Fragment, useState, } from 'react';
+import Axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+  const [state,setState]=useState([]);
+
+  const [loading,setLoading]=useState(false)
+
+  
+
+const apiCall= ()=>{
+
+
+Axios.get(" https://reqres.in/api/users?page=1 ").then( (data)=>{ console.log(data);
+
+const newData=data.data.data;
+
+console.log(newData)
+
+setState(newData);
+
+setLoading(true);
+  
+} )
+
+
+
+}
+
+    return (
+
+      
+      <Fragment>
+
+                           
+          
+        <div className="container p-3">
+         
+        
+         {/* NavBar   */}
+
+           <nav className="navbar navbar-expand-lg fixed-top  navbar-light bg-dark shadow " >
+               <div className="container ">
+                 <a className="navbar-brand text-white" href="#"><b>Zippy</b></a>
+                
+                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                   <div className="navbar-nav mx-md-auto ">
+                     <a className="nav-link active text-white bi bi-arrow-down-circle fs-2 " aria-current="page" href="#"  type="button" onClick={apiCall} >Get Data</a>
+    
+                   </div>
+                 </div>
+               </div>
+           </nav>
+
+        </div>
+        
+        <h2 className="alert alert-info mt-5 text-center"> User Information </h2>
+        
+        <div className='row text center'>
+
+       
+
+        
+
+                             
+                                {
+                                   loading ? (
+
+
+
+                                    state.map( (curElement,i)=>{  
+                                      return(
+                                                   <div>
+                                                                             
+                                                        <div className="col-10 col-md-6 mt-5 text-center" >
+                                                          
+                                                          <div className="card text-center border-5 border-danger mx-5" >
+                                                              
+                                                           <figure  className="figure-img img-fluid rounded "  >
+                                                            <img src={curElement.avatar} className="figure-img img-fluid rounded" alt="..." height="350px" width="600px"  ></img>
+                                                           </figure>
+                       
+                                                          
+                                                          <h6 > ID:{curElement.id} </h6>
+                                                          <h6 >Name: {curElement.first_name +" "+ curElement.last_name}</h6>
+                                                          <h6 >Email: {curElement.email} </h6>
+                       
+                                                          </div>
+                                                           
+                                                        </div>
+                       
+                       
+                                                   </div>
+                                    
+                                      )
+                                } )
+
+
+                                    ):(
+
+                                    <div className="spinner-border text-warning mx-auto" role={loading} >
+                                      <span className="visually-hidden" > </span>
+                                    </div>
+                                   )
+                                   
+                                 }                
+                             
+
+        </div>
+
+      </Fragment>
+    );
+
+
+
 }
 
 export default App;
